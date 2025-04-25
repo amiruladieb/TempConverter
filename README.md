12ff5a4c8bccd11e163a298fa573c814fa461c60
adieb <mrizqiamiruladieb@gmail.com>
2025-04-25 22:11:55 +0700

    feat: Only numbers and one decimal point allow on jtextfield

---
+++ README.md
@@ -0,0 +1,8 @@
+# TempConverter
+Java
+
+- **IDE**: NetBeans
+
+## Problem and Solution
+- Only *numbers* and one decimal *point* allow on **JTextField**: [reference1](https://youtu.be/OVaGcvuNVxk?si=Pwd0dGsDjSwKk5QX), [reference2](https://www.tutorialspoint.com/how-can-we-make-jtextfield-accept-only-numbers-in-java)
+
--- src/CelsiusPage.java
+++ src/CelsiusPage.java
@@ -12,9 +12,14 @@ class CelsiusPage implements ActionListener {
     JLabel input2Label;
     JComboBox inputTempField;
     JButton submitButton;
+    JLabel notifLabel;
+    JLabel notif2Label;
     JLabel outputLabel;
     JTextField outputField;
     JLabel outputTempLabel;
+    
+    String inputTemp;
+    double num, result;
     
     //construct
     CelsiusPage() {
@@ -32,35 +37,74 @@ class CelsiusPage implements ActionListener {
         inputField.setBounds(180, 28, 190, 50);
         inputField.setFont(new Font("Comic Sans", Font.PLAIN, 24));
         
+        //inputField accept only numbers, dot, backspace, delete, left and right arrows keys
+        inputField.addKeyListener(new KeyAdapter() {
+            
+            public void keyTyped(KeyEvent evt) {
+                
+                if (Character.isLetter(evt.getKeyChar())) {
+                    evt.consume();
+                }
+                else if (inputField.getText().isEmpty()) {
+                        notifLabel.setVisible(true);
+                }
+                else {
+                    
+                    try {
+                        Double.parseDouble(inputField.getText() + evt.getKeyChar());
+                        notifLabel.setVisible(false);
+                    }
+                    catch (NumberFormatException e) {
+                        evt.consume();
+                    }
+                    
+                }
+                
+            }
+            
+        });
+        
         inputTempLabel = new JLabel("°C");
         inputTempLabel.setBounds(385, 40, 30, 30);
         inputTempLabel.setFont(new Font("Comic Sans", Font.PLAIN, 20));
         
+        notifLabel = new JLabel("Masukan suhu awal terlebih dahulu !");
+        notifLabel.setBounds(40, 85, 280, 20);
+        notifLabel.setFont(new Font("Comic Sans", Font.ITALIC, 16));
+        notifLabel.setForeground(Color.red);
+        
         input2Label = new JLabel("Konversikan menjadi");
-        input2Label.setBounds(40, 120, 180, 30);
+        input2Label.setBounds(40, 130, 180, 30);
         input2Label.setFont(new Font("Comic Sans", Font.PLAIN, 20));
         
-        String[] temp = {"°F", "K", "°R"};
+        String[] temp = {" ", "°F", "K", "°R"};
         inputTempField = new JComboBox(temp);
-        inputTempField.setBounds(245, 112, 50, 40);
+        inputTempField.setBounds(245, 125, 50, 40);
         inputTempField.setFont(new Font("Comic Sans", Font.PLAIN, 18));
+        inputTempField.addActionListener(this);
         
         submitButton = new JButton("Kirim");
-        submitButton.setBounds(330, 112, 80, 40);
+        submitButton.setBounds(330, 125, 80, 40);
         submitButton.setFont(new Font("Comic Sans", Font.PLAIN, 18));
         submitButton.setFocusable(false);
+        submitButton.addActionListener(this);
+        
+        notif2Label = new JLabel("Silakan pilih suhu tujuan dahulu !");
+        notif2Label.setBounds(40, 175, 250, 20);
+        notif2Label.setFont(new Font("Comic Sans", Font.ITALIC, 16));
+        notif2Label.setForeground(Color.red);
         
         outputLabel = new JLabel("Hasilnya :");
-        outputLabel.setBounds(40, 220, 150, 30);
+        outputLabel.setBounds(40, 230, 150, 30);
         outputLabel.setFont(new Font("Comic Sans", Font.PLAIN, 20));
         
         outputField = new JTextField();
-        outputField.setBounds(180, 212, 190, 50);
+        outputField.setBounds(180, 222, 190, 50);
         outputField.setFont(new Font("Comic Sans", Font.PLAIN, 24));
         outputField.setEditable(false);
         
-        outputTempLabel = new JLabel("°C");
-        outputTempLabel.setBounds(385, 220, 30, 30);
+        outputTempLabel = new JLabel();
+        outputTempLabel.setBounds(385, 230, 30, 30);
         outputTempLabel.setFont(new Font("Comic Sans", Font.PLAIN, 20));
         
         frame.add(inputLabel);
@@ -69,6 +113,8 @@ class CelsiusPage implements ActionListener {
         frame.add(input2Label);
         frame.add(inputTempField);
         frame.add(submitButton);
+        frame.add(notifLabel);
+        frame.add(notif2Label);
         frame.add(outputLabel);
         frame.add(outputField);
         frame.add(outputTempLabel);
@@ -80,6 +126,63 @@ class CelsiusPage implements ActionListener {
     //method
     @Override
     public void actionPerformed(ActionEvent e) {
+        
+        if (e.getSource() == inputTempField) {
+            
+            inputTemp = String.valueOf(inputTempField.getSelectedItem());
+            
+            switch (inputTemp) {
+                case " ":
+                    notif2Label.setVisible(true);
+                    break;
+                case "°F":
+                    notif2Label.setVisible(false);
+                    break;
+                case "K":
+                    notif2Label.setVisible(false);
+                    break;
+                case "°R":
+                    notif2Label.setVisible(false);
+                    break;
+            }
+            
+            outputField.setText("");
+            outputTempLabel.setText(inputTemp);
+//            System.out.println(inputTemp);
+            
+        }
+        
+        if (e.getSource() == submitButton) {
+            
+            if (inputField.getText().isEmpty()) {
+//                JOptionPane.showMessageDialog(null, "isian kosong");
+                notifLabel.setVisible(true);
+                outputField.setText("");
+            }
+            
+            num = Double.parseDouble(inputField.getText());
+//            System.out.println(num);
+            
+            switch (inputTemp) {
+                case " ":
+                    result = Double.parseDouble("");
+                    break;
+                case "°F":
+                    result = ((num * 9) / 5) + 32;
+                    break;
+                case "K":
+                    result = num + 273.15;
+                    break;
+                case "°R":
+                    result = (num * 4) / 5;
+                    break;
+            }
+            
+            notifLabel.setVisible(false);
+            notif2Label.setVisible(false);
+            outputField.setText(String.valueOf(result));
+            
+        }
     }
     
 }
